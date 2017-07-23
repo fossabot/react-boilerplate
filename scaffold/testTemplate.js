72d6e914a3638eec4d060e6d45dea2a30203b902
Jon Gunnison <jmgunnison@gmail.com>
2017-07-23 09:59:34 -0700

    Cleaning up Enzyme import in the Test Template

--- scaffold/testTemplate.js
+++ scaffold/testTemplate.js
@@ -1,5 +1,5 @@
 module.exports = function (name) { return (`import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 import ${name} from './';
 
 describe('Component: ${name}', () => {
